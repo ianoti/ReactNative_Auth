a65b079404d6ca9365fb7c3b42eb6306e1c57ecc
ianoti <ianodhiambootieno@gmail.com>
2017-08-24 11:51:30 +0300

    make input reusable
    1. boilerplate for input component
    2. make input available to other files
    3. setup input to be a controlled input in the login form

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(text) {
+    console.log('I have modified', text.target.name);
+    // this.setState({ emailInput: text });
+  }
+
+
   render() {
+    console.log('Woopeee', this.state);
     return (
       <Card>
-        <CardSection></CardSection>
-        <CardSection></CardSection>
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 20, width: 200 }}
+            placeholder="email input"
+          />
+        </CardSection>
+        <CardSection>
+          <TextInput
+            data-name="passwordInput"
+            style={{ height: 20, width: 200 }}
+            placeholder="password input"
+          />
+        </CardSection>
         <CardSection>
           <Button>
             Login
