4477d6e05d12609654a38eae2ced2d6255f94c05
ianoti <ianodhiambootieno@gmail.com>
2017-08-24 16:46:15 +0300

    conditional rendering
    1. add comments on using one helper method to update two pieces of state
    2. rudimentary loading icon display using state

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
 
-import { Button, Card, CardSection, CustomInput } from './common';
+import { Button, Card, CardSection, CustomInput, Spinner } from './common';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: '',
       email: '',
       password: '',
-      signinError: ''
+      error: '',
+      loading: false
     };
 
     this.onButtonPress = this.onButtonPress.bind(this);
   }
 
   onButtonPress() {
-    this.setState({ signinError: '' });
+    this.setState({ error: '', loading: true });
     const { email, password } = this.state;
     firebase.auth().signInWithEmailAndPassword(email, password)
       .catch(() => {
@@ -28,13 +30,23 @@ class LoginForm extends Component {
       });
   }
 
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size='small' />;
+    }
+    return (
+      <Button onPress={this.onButtonPress}>
+        Login
+      </Button>
+    );
+  }
+
   render() {
     const { errorTextStyle } = styles;
     return (
       <Card>
         <CardSection>
           <CustomInput
-            value={this.state.email}
             onChangeText={email => this.setState({ email })}
             label='Email'
             placeholder='user@example.com'
@@ -42,7 +54,6 @@ class LoginForm extends Component {
         </CardSection>
         <CardSection>
           <CustomInput
-            value={this.state.password}
             onChangeText={password => this.setState({ password })}
             label='password'
             placeholder='password'
@@ -53,14 +64,33 @@ class LoginForm extends Component {
           <Text style={errorTextStyle}>{this.state.signinError}</Text>
         </CardSection>
         <CardSection>
-          <Button onPress={this.onButtonPress}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
   }
 }
+/*
+To change set state of both fields using one helper method set up customInput like so
+<CustomInput
+  onChangeText={this.onTextChange.bind(this, 'email')}
+  label='Email'
+  placeholder='user@example.com'
+/>
+and the the helper method looks like so
+onTextChange(keyId, text) {
+  console.log('hello', text, keyId);
+  if (keyId===email){
+  this.setState({ email: text})
+  }
+  if (keyId===password){
+  this.setState({ password: text})
+  }
+
+}
+
+this can then be paired with a setState to each needed piece of state
+*/
 
 const styles = {
   errorTextStyle: {
